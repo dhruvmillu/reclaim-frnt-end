483f3fa0cec22ddda93bad8ebb506171d72d64e0
Dhruv Millu <dhruvmillu02@gmail.com>
2023-04-03 16:47:02 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIgW0Z9ycGPx9N_mQrZnBTDup6aMIJnYM",
+  authDomain: "reclaim-protocol.firebaseapp.com",
+  projectId: "reclaim-protocol",
+  storageBucket: "reclaim-protocol.appspot.com",
+  messagingSenderId: "755849041357",
+  appId: "1:755849041357:web:357268ba657522bb996084",
+  measurementId: "G-CKLRWRVXQE"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
